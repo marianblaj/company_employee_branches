600648e1e40c4d17e7ef48cd49020605f1193452
Marian <marian.blaj@yahoo.com>
2020-02-12 18:05:04 +0200

    master nou

--- src/main/java/com/example/company_employee/company_employee_branches/services/Company.java
+++ src/main/java/com/example/company_employee/company_employee_branches/services/Company.java
@@ -6,10 +6,7 @@ import lombok.Data;
 public class Company {
     private String name;
     private String address;
-    private String cif;
-    private String test3;
-    private String test5;
-    private String test6;
+
 
 
     }
