f34aa3e313852c31612660fb05ad828a6d16f6d1
Marian <marian.blaj@yahoo.com>
2020-02-12 17:22:10 +0200

    commit after 1

--- src/main/java/com/example/company_employee/company_employee_branches/services/Company.java
+++ src/main/java/com/example/company_employee/company_employee_branches/services/Company.java
@@ -1,13 +1,14 @@
 package com.example.company_employee.company_employee_branches.services;
 
 import lombok.Data;
+import lombok.Getter;
+
 
 @Data
 public class Company {
     private String name;
     private String address;
-    private String hh;
+    private String change;
 
 
-    }
 }
