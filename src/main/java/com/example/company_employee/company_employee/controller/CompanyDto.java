dcd7def636492c3ab8a4e025c6a6ae8ea9ad525c
Marian <marian.blaj@yahoo.com>
2020-02-12 11:17:01 +0200

    created company class

--- src/main/java/com/example/company_employee/company_employee/controller/CompanyDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.company_employee.company_employee.controller;
-
-import lombok.Data;
-
-@Data
-public class CompanyDto {
-    private String name;
-    private String cui;
-}
---
+++ src/main/java/com/example/company_employee/company_employee_branches/services/Company.java
@@ -0,0 +1,11 @@
+package com.example.company_employee.company_employee_branches.services;
+
+import lombok.Data;
+
+@Data
+public class Company {
+    private String name;
+    private String address;
+    private String cif;
+
+}
